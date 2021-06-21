d77055eab42a28456fdd05d549f39266020bef14
Joe Ranweiler <joe@lemma.co>
2021-06-20 20:24:44 -0700

    Add syscall tracepoint test (#34)
    - Update the `syscalls` example with tracee-quieting and TSV output options (to support testing)
    - Add scripts for testing syscall tracing by comparing against kernel tracepoint events
    - Run integration test in CI task

--- examples/syscalls.rs
+++ examples/syscalls.rs
@@ -1,40 +1,46 @@
 use std::collections::BTreeMap;
-use std::env;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
+use anyhow::Result;
 use pete::{Ptracer, Restart, Stop, Tracee};
+use lazy_static::lazy_static;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(short, long)]
+    tsv: bool,
 
-fn main() -> anyhow::Result<()> {
-    let syscalls = load_syscalls();
+    #[structopt(short, long)]
+    quiet: bool,
 
-    let argv: Vec<String> = env::args().skip(1).collect();
+    #[structopt(min_values = 1)]
+    argv: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    let argv: Vec<String> = opt.argv;
+
     let mut cmd = Command::new(&argv[0]);
-    cmd.args(&argv[1..]);
+
+    if let Some(args) = argv.get(1..) {
+        cmd.args(args);
+    }
+
+    if opt.quiet {
+        cmd.stdout(Stdio::null());
+        cmd.stderr(Stdio::null());
+    }
 
     let mut ptracer = Ptracer::new();
     let _child = ptracer.spawn(cmd)?;
 
-    while let Some(tracee) = ptracer.wait()? {
-        let regs = tracee.registers()?;
-        let pc = regs.rip as u64;
-
-        match tracee.stop {
-            Stop::SyscallEnterStop(..) |
-            Stop::SyscallExitStop(..)=> {
-                let rax = regs.orig_rax;
-                let syscall = syscalls
-                    .get(&rax)
-                    .cloned()
-                    .unwrap_or_else(|| format!("unknown (rax = 0x{:x})", rax));
-
-                let Tracee { pid, stop, .. } = tracee;
-                println!("pid = {}, pc = {:x}: [{}], {:?}", pid, pc, syscall, stop);
-            },
-            _ => {
-                let Tracee { pid, stop, .. } = tracee;
-                println!("pid = {}, pc = {:x}: {:?}", pid, pc, stop);
-            },
+    while let Some(mut tracee) = ptracer.wait()? {
+        if opt.tsv {
+            on_stop_tsv(&mut tracee)?;
+        } else {
+            on_stop(&mut tracee)?;
         }
 
         ptracer.restart(tracee, Restart::Syscall)?;
@@ -43,9 +49,64 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn on_stop(tracee: &mut Tracee) -> Result<()> {
+    let regs = tracee.registers()?;
+    let pc = regs.rip as u64;
+
+    match tracee.stop {
+        Stop::SyscallEnterStop(..) |
+        Stop::SyscallExitStop(..) => {
+            let syscallno = regs.orig_rax;
+            let syscall = SYSCALL_TABLE
+                .get(&syscallno)
+                .cloned()
+                .unwrap_or_else(|| format!("unknown (syscallno = 0x{:x})", syscallno));
+
+            let Tracee { pid, stop, .. } = tracee;
+            println!("pid = {}, pc = {:x}: [{}], {:?}", pid, pc, syscall, stop);
+        },
+        _ => {
+            let Tracee { pid, stop, .. } = tracee;
+            println!("pid = {}, pc = {:x}: {:?}", pid, pc, stop);
+        },
+    }
+
+    Ok(())
+}
+
+fn on_stop_tsv(tracee: &mut Tracee) -> Result<()> {
+    match tracee.stop {
+        Stop::SyscallEnterStop(..) => {
+            on_syscall_stop_tsv(tracee, true)?;
+        }
+        Stop::SyscallExitStop(..)=> {
+            on_syscall_stop_tsv(tracee, false)?;
+        }
+        _ => {},
+    }
+
+    Ok(())
+}
+
+fn on_syscall_stop_tsv(tracee: &mut Tracee, syscall_enter: bool) -> Result<()> {
+    let regs = tracee.registers()?;
+    let syscallno = regs.orig_rax;
+    let stop_type = if syscall_enter { "enter" } else { "exit" };
+
+    println!("{}\t{}\t{}", tracee.pid, stop_type, syscallno);
+
+    Ok(())
+}
+
 const SYSCALLS: &'static str = include_str!("data/syscalls_x64.tsv");
 
-fn load_syscalls() -> BTreeMap<u64, String> {
+lazy_static! {
+    static ref SYSCALL_TABLE: SyscallTable = load_syscall_table();
+}
+
+type SyscallTable = BTreeMap<u64, String>;
+
+fn load_syscall_table() -> SyscallTable {
     let mut syscalls = BTreeMap::new();
 
     for line in SYSCALLS.split_terminator('\n') {
